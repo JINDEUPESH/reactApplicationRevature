8dcb8a40336d674bec1cf21ea0b69518a7920902
JINDEUPESH <jindeupesh888@gmail.com>
2025-10-03 01:34:58 +0530

    update 2

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,12 +1,52 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+
 
 function Welcome() {
+  const key = import.meta.env.VITE_API_KEY;
+  const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${key}`;
 
-    
+  const [article, setArticle] = useState(null);
 
-    return(
-        <h1>Welcome to Incidents! Click on incidents button to get incident List</h1>
-    );
+  const randomIndex = Math.floor(Math.random() * 20); 
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    async function fetchData() {
+      try {
+        let results = await fetch(url, { signal });
+        let parsedResponse = await results.json();
+        setArticle(parsedResponse.articles[randomIndex]);
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          console.error("Error fetching news:", err);
+        }
+      }
+    }
+
+
+    fetchData();
+
+    return () => controller.abort();
+  }, [url]);
+
+  return (
+    <>
+      <h1>Top Headline</h1>
+      {article ? (
+        <div>
+          <h4>{article.title}</h4>
+          <p>{article.description}</p>
+          <a href={article.url} target="_blank">
+            Read more
+          </a>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
+  );
 }
 
 export default Welcome;
